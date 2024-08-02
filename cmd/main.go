7b3e1aec8de10ae0e705c3b6daf3637d3c48e86b
skrollbrad <cliquebenito@mail.ru>
2024-08-02 20:37:14 +0300

    csv

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"test/config"
 	"test/internal/models"
 	"test/pkg"
@@ -29,8 +32,105 @@ func getItemsHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, people)
 }
 
+func convertCsvtoJson() error {
+	csvFile, err := os.Open("ueba(3).csv")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer csvFile.Close()
+
+	reader := csv.NewReader(csvFile)
+	reader.FieldsPerRecord = -1
+
+	csvData, err := reader.ReadAll()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var emp models.User
+	var employees []models.User
+
+	for _, each := range csvData {
+		emp.ID, _ = strconv.Atoi(each[0])
+		emp.UID = each[1]
+		emp.Domain = each[2]
+		emp.CN = each[3]
+		emp.Department = each[4]
+		emp.Title = each[5]
+		emp.Who = each[6]
+		emp.LogonCount, _ = strconv.Atoi(each[7])
+		emp.NumLogons7, _ = strconv.Atoi(each[8])
+		emp.NumShare7, _ = strconv.Atoi(each[9])
+		emp.NumFile7, _ = strconv.Atoi(each[10])
+		emp.NumAD7, _ = strconv.Atoi(each[11])
+		emp.NumN7, _ = strconv.Atoi(each[12])
+		emp.NumLogons14, _ = strconv.Atoi(each[13])
+		emp.NumShare14, _ = strconv.Atoi(each[14])
+		emp.NumFile14, _ = strconv.Atoi(each[15])
+		emp.NumAD14, _ = strconv.Atoi(each[16])
+		emp.NumN14, _ = strconv.Atoi(each[17])
+		emp.NumLogons30, _ = strconv.Atoi(each[18])
+		emp.NumShare30, _ = strconv.Atoi(each[19])
+		emp.NumFile30, _ = strconv.Atoi(each[20])
+		emp.NumAD30, _ = strconv.Atoi(each[21])
+		emp.NumN30, _ = strconv.Atoi(each[22])
+		emp.NumLogons150, _ = strconv.Atoi(each[23])
+		emp.NumShare150, _ = strconv.Atoi(each[24])
+		emp.NumFile150, _ = strconv.Atoi(each[25])
+		emp.NumAD150, _ = strconv.Atoi(each[26])
+		emp.NumN150, _ = strconv.Atoi(each[27])
+		emp.NumLogons365, _ = strconv.Atoi(each[28])
+		emp.NumShare365, _ = strconv.Atoi(each[29])
+		emp.NumFile365, _ = strconv.Atoi(each[30])
+		emp.NumAD365, _ = strconv.Atoi(each[31])
+		emp.NumN365, _ = strconv.Atoi(each[32])
+		emp.HasUserPrincipalName, _ = strconv.ParseBool(each[33])
+		emp.HasMail, _ = strconv.ParseBool(each[34])
+		emp.HasPhone, _ = strconv.ParseBool(each[35])
+		emp.FlagDisabled, _ = strconv.ParseBool(each[36])
+		emp.FlagLockout, _ = strconv.ParseBool(each[37])
+		emp.FlagPasswordNotRequired, _ = strconv.ParseBool(each[38])
+		emp.FlagPasswordCantChange, _ = strconv.ParseBool(each[39])
+		emp.FlagDontExpirePassword, _ = strconv.ParseBool(each[40])
+		emp.OwnedFiles, _ = strconv.Atoi(each[41])
+		emp.NumMailboxes, _ = strconv.Atoi(each[42])
+		emp.NumMemberOfGroups, _ = strconv.Atoi(each[43])
+		emp.NumMemberOfIndirectGroups, _ = strconv.Atoi(each[44])
+		emp.MemberOfIndirectGroupsIDs = each[45]
+		emp.MemberOfGroupsIDs = each[46]
+		emp.IsAdmin, _ = strconv.ParseBool(each[47])
+		emp.IsService, _ = strconv.ParseBool(each[48])
+
+		employees = append(employees, emp)
+	}
+
+	jsonData, err := json.Marshal(employees)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println(string(jsonData))
+
+	jsonFile, err := os.Create("newdata.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+
+	jsonFile.Write(jsonData)
+	jsonFile.Close()
+	return nil
+}
+
 func getPersonByIDs(ids []int) ([]models.Person, error) {
-	filename := "data.json"
+	err := convertCsvtoJson()
+	if err != nil {
+		return []models.Person{}, err
+	}
+
+	filename := "newdata.json"
 	file, err := os.Open(filename)
 	if err != nil {
 		return []models.Person{}, err
@@ -56,7 +156,7 @@ func getPersonByIDs(ids []int) ([]models.Person, error) {
 	}
 
 	if len(result) == 0 {
-		return nil, errors.New("People not found")
+		return nil, errors.New("Info not found")
 	}
 
 	return result, nil
--- internal/models/dto.go
+++ internal/models/dto.go
@@ -3,9 +3,83 @@ package models
 type Input struct {
 	ID []int `json:"id" binding:"required"`
 }
+
+//	type Person struct {
+//		ID         int    `json:"id"`
+//		Name       string `json:"name"`
+//		Occupation string `json:"occupation"`
+//		Born       string `json:"born"`
+//	}
 type Person struct {
-	ID         int    `json:"id"`
-	Name       string `json:"name"`
-	Occupation string `json:"occupation"`
-	Born       string `json:"born"`
+	Number      int    `json:"number"`
+	ID          int    `json:"id"`
+	UID         string `json:"uid"`
+	Domain      string `json:"domain"`
+	CN          string `json:"cn"`
+	Department  string `json:"department"`
+	Title       string `json:"title"`
+	Who         string `json:"who"`
+	LogonCount  int    `json:"logon_count"`
+	NumLogons7  int    `json:"num_logons_7"`
+	NumShare7   int    `json:"num_share_7"`
+	NumFile7    int    `json:"num_file_7"`
+	NumAd7      int    `json:"num_ad_7"`
+	NumN7       int    `json:"num_n_7"`
+	NumLogons14 int    `json:"num_logons_14"`
+	NumShare14  int    `json:"num_share_14"`
+	NumFile14   int    `json:"num_file_14"`
+	NumAd14     int    `json:"num_ad_14"`
+	NumN14      int    `json:"num_n_14"`
+}
+
+type User struct {
+	ID                        int
+	UID                       string
+	Domain                    string
+	CN                        string
+	Department                string
+	Title                     string
+	Who                       string
+	LogonCount                int
+	NumLogons7                int
+	NumShare7                 int
+	NumFile7                  int
+	NumAD7                    int
+	NumN7                     int
+	NumLogons14               int
+	NumShare14                int
+	NumFile14                 int
+	NumAD14                   int
+	NumN14                    int
+	NumLogons30               int
+	NumShare30                int
+	NumFile30                 int
+	NumAD30                   int
+	NumN30                    int
+	NumLogons150              int
+	NumShare150               int
+	NumFile150                int
+	NumAD150                  int
+	NumN150                   int
+	NumLogons365              int
+	NumShare365               int
+	NumFile365                int
+	NumAD365                  int
+	NumN365                   int
+	HasUserPrincipalName      bool
+	HasMail                   bool
+	HasPhone                  bool
+	FlagDisabled              bool
+	FlagLockout               bool
+	FlagPasswordNotRequired   bool
+	FlagPasswordCantChange    bool
+	FlagDontExpirePassword    bool
+	OwnedFiles                int
+	NumMailboxes              int
+	NumMemberOfGroups         int
+	NumMemberOfIndirectGroups int
+	MemberOfIndirectGroupsIDs string
+	MemberOfGroupsIDs         string
+	IsAdmin                   bool
+	IsService                 bool
 }
